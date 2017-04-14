a05191053828a42211b3cd86ffe98d283501c359
vincenttchang90 <vincenttchang90@gmail.com>
2017-04-14 16:57:19 -0400

    Added projects, education, bio, work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,85 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/*work contains an array of jobs. Each object in the jobs array should contain an employer, title, location, dates worked and description strings.
+
+projects contains an array of projects. Each object in the projects array should contain title, dates and description strings, and an images array with URL strings for project images.
+
+bio contains name, role, welcomeMessage, and biopic strings, contacts object and skills array of skill strings. The contacts object should contain a mobile number, email address, github username, twitter handle 
+and location. The twitter property is optional.
+
+education contains an array of schools. Each object in the schools array contains name, location, degree dates and url strings, and amajors array of major strings. 
+education also contains an onlineCourses array. Each object in the onlineCourses array should contain a title, school, dates and url strings.
+*/
+
+var work = {
+	jobs : [
+		{
+			employer : "KSI",
+			title : "Account Receivable",
+			location : "South Plainfield, NJ",
+			dates : "2016-2017",
+			description : "Managed over 300 accounts"
+		},
+		{
+			employer : "Self",
+			title : "Professional Poker Player",
+			location : "NJ",
+			dates : "2013-2016",
+			description : "Live and online tournament professional"
+		}
+	]
+};
+
+var projects = {
+	project : [
+			{
+				title : "Will update",
+				dates : "Will update",
+				description : "Will update,",
+				images : "Will update"
+			}]
+};
+
+var bio = {
+	name : "Vincent Chang",
+	role : "Professional",
+	welcomeMessage : "You can't win what you don't put in the middle",
+	biopic : "images/me.png",
+	contacts : {
+		mobile : "732-233-7534",
+		email : "vincenttchang@gmail.com",
+		github : "vincenttchang",
+		twitter : "NA",
+		location : "Princeton, NJ"
+	},
+	skills : ["Web Dev", "NLE", "Tennis", "Olympic weightlifting"]
+};
+
+var education = {
+	schools : [
+				{
+					name : "Rutgers University",
+					location : "New Brunswick, NJ",
+					degree : "BS",
+					date : "2015",
+					majors : ["Economics", "Math"],
+					url : "NA"
+				}
+			],
+	onlineCourses : {
+					title : "Front End Web Dev Nanodegree",
+					school : "Udacity",
+					dates : "2017"
+					}
+}
+
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+
+	var formattedSkill;
+	for (var i = 0; i < bio.skills.length; i++) {
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+		$("#skills").append(formattedSkill);
+	}
+};
+
+var formattedHTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedHTMLheaderName);
