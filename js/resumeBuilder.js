cc858ca01976bce7c62a3a9c62ebd82e29b335eb
Vincent Chang <vincenttchang90@gmail.com>
2017-04-16 19:26:35 -0400

    testing home pc

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,10 +2,10 @@
 
 projects contains an array of projects. Each object in the projects array should contain title, dates and description strings, and an images array with URL strings for project images.
 
-bio contains name, role, welcomeMessage, and biopic strings, contacts object and skills array of skill strings. The contacts object should contain a mobile number, email address, github username, twitter handle 
+bio contains name, role, welcomeMessage, and biopic strings, contacts object and skills array of skill strings. The contacts object should contain a mobile number, email address, github username, twitter handle
 and location. The twitter property is optional.
 
-education contains an array of schools. Each object in the schools array contains name, location, degree dates and url strings, and amajors array of major strings. 
+education contains an array of schools. Each object in the schools array contains name, location, degree dates and url strings, and amajors array of major strings.
 education also contains an onlineCourses array. Each object in the onlineCourses array should contain a title, school, dates and url strings.
 */
 
@@ -99,4 +99,4 @@ for (job in work.jobs) {
 	$(".work-entry:last").append(formattedEmployerTitle).append(formattedDates).append(formattedDescription);
 };
 
-//github push testss
+//github push testsss
